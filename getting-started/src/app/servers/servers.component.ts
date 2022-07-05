341fd1e0cacbd3eb0753e1c5bde0487fb4592e6c
Mateusz Ziebura <mateusz_860@op.pl>
2022-07-05 19:37:32 +0200

    Event Binding

--- getting-started/src/app/servers/servers.component.ts
+++ getting-started/src/app/servers/servers.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
+  serverCreationStatus = 'No serwer was created';
+
   constructor() {
     setTimeout(() => {
       this.allowStartNewServer = true;
     }, 2000);
+  }
+
+  onServerCreates() {
+    this.serverCreationStatus = 'Server Created';
   }
 
   allowStartNewServer = false;
