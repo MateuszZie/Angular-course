9be8d7bb5ed95bccfd53e41fdef0241ebc4ef4e6
Mateusz Ziebura <mateusz_860@op.pl>
2022-07-16 08:18:40 +0200

    Displaying Recipe Details

--- basic-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ basic-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'Recipe Description',
+      'https://img.delicious.com.au/ZCVFCYn_/del/2022/02/chicken-chickpea-curry-163942-1.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
