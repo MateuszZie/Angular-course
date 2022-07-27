623a3349c2073144c04ce006078783e4d24343a2
Mateusz Ziebura <mateusz_860@op.pl>
2022-07-27 10:20:13 +0200

    Adding Navigation with Event Binding and ngIf

--- basic-project/src/app/header/header.component.ts
+++ basic-project/src/app/header/header.component.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class Header {}
+export class Header {
+  @Output() display = new EventEmitter<string>();
+
+  changeView(viewName: string): void {
+    this.display.emit(viewName);
+  }
+}
