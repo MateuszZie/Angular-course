a9e21112d40d0666f56abdfbb912056a8f8560b8
Mateusz Ziebura <mateusz_860@op.pl>
2022-08-11 19:57:34 +0200

    Closing the Dropdown From Anywhere

--- basic-project/src/app/shered/dropdown.directive.ts
+++ basic-project/src/app/shered/dropdown.directive.ts
@@ -1,12 +1,21 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+} from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]',
 })
 export class DropdownDirective {
-  @HostBinding('class.open') open = false;
+  constructor(private elRef: ElementRef) {}
 
-  @HostListener('click') clcked() {
-    this.open = !this.open;
+  @HostBinding('class.open') isOpen = false;
+
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target)
+      ? !this.isOpen
+      : false;
   }
 }
