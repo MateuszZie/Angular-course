325bee37ab7ea1c9ba0f12fe6c57a7f2fa9f0a3f
Mateusz Ziebura <mateusz_860@op.pl>
2022-08-23 20:13:55 +0200

    Fixing Page Reload Issues

--- basic-project/src/app/app-routing.module.ts
+++ basic-project/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 const appRouts: Routes = [
   { path: '', redirectTo: '/recipies', pathMatch: 'full' },
-  { path: 'recipies', component: RecipesComponent },
+  {
+    path: 'recipies',
+    component: RecipesComponent,
+    children: [{ path: 'detail', component: RecipeDetailComponent }],
+  },
   { path: 'shopping-list', component: ShoppingListComponent },
 ];
 
