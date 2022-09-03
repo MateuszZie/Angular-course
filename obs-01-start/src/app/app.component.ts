b1dd1df65613a957f22e88bd42f46b76b516cd7a
Mateusz Ziebura <mateusz_860@op.pl>
2022-09-03 12:19:32 +0200

    Subjects

--- obs-01-start/src/app/app.component.ts
+++ obs-01-start/src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { UserService } from "./user.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
-  constructor() {}
+  constructor(private userService: UserService) {}
 
-  ngOnInit() {}
+  userActivated = false;
+
+  ngOnInit() {
+    this.userService.mySubject.subscribe((data) => (this.userActivated = data));
+  }
+
+  ngOnDestroy(): void {
+    this.userService.mySubject.unsubscribe();
+  }
 }
