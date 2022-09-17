f4317a96a31e40eff252d7031c0140421fce75b7
Mateusz Ziebura <mateusz_860@op.pl>
2022-09-17 12:20:40 +0200

    Validating User Input

--- basic-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ basic-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Ingridient } from 'src/app/shered/ingredient.model';
 import { RecipeService } from '../recipe.service';
@@ -46,8 +46,11 @@ export class RecipeEditComponent implements OnInit {
         for (let ingredient of recipe.ingredients) {
           recipeIngredients.push(
             new FormGroup({
-              name: new FormControl(ingredient.name),
-              amount: new FormControl(ingredient.amount),
+              name: new FormControl(ingredient.name, Validators.required),
+              amount: new FormControl(ingredient.amount, [
+                Validators.required,
+                Validators.pattern(/^[1-9]+[0-9]*$/),
+              ]),
             })
           );
         }
@@ -55,8 +58,8 @@ export class RecipeEditComponent implements OnInit {
     }
 
     this.recipeForm = new FormGroup({
-      name: new FormControl(recipeName),
-      imgPath: new FormControl(recipeImagePath),
+      name: new FormControl(recipeName, Validators.required),
+      imgPath: new FormControl(recipeImagePath, Validators.required),
       description: new FormControl(recipeDescription),
       ingredients: recipeIngredients,
     });
@@ -69,8 +72,11 @@ export class RecipeEditComponent implements OnInit {
   addIngredient() {
     (<FormArray>this.recipeForm.get('ingredients')).push(
       new FormGroup({
-        name: new FormControl(null),
-        amount: new FormControl(null),
+        name: new FormControl(null, Validators.required),
+        amount: new FormControl(null, [
+          Validators.required,
+          Validators.pattern(/^[1-9]+[0-9]*$/),
+        ]),
       })
     );
   }
