4a33400542b02c534378361bf9cc185c24f73f5b
Mateusz Ziebura <mateusz_860@op.pl>
2022-09-18 17:33:25 +0200

    Sending a POST Request

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
+  private static HTTP_ADRES =
+    "https://ng-complete-guide-ce1ed-default-rtdb.europe-west1.firebasedatabase.app/";
+  private static SEND_POST = AppComponent.HTTP_ADRES + "posts.json";
+
   loadedPosts = [];
 
   constructor(private http: HttpClient) {}
@@ -15,7 +19,9 @@ export class AppComponent implements OnInit {
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    console.log(postData);
+    this.http
+      .post(AppComponent.SEND_POST, postData)
+      .subscribe((response) => console.log(response));
   }
 
   onFetchPosts() {
