81956ba0a6f796f7acb9f2eacdbad6b0ecbaf2ac
Mateusz Ziebura <mateusz_860@op.pl>
2022-09-18 12:51:11 +0200

    Practicing Pipes

--- pipes-start/src/app/app.module.ts
+++ pipes-start/src/app/app.module.ts
@@ -5,9 +5,11 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { ShortenPipe } from "./shorten.pipe";
 import { FilterPipe } from './filter.pipe';
+import { SortPipe } from './sort.pipe';
+import { ReversePipe } from './reverse.pipe';
 
 @NgModule({
-  declarations: [AppComponent, ShortenPipe, FilterPipe],
+  declarations: [AppComponent, ShortenPipe, FilterPipe, SortPipe, ReversePipe],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
