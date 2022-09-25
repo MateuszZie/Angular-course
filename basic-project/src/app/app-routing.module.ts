dd353819a00b9a070f3f82e1f67029515e9075c4
Mateusz Ziebura <mateusz_860@op.pl>
2022-09-25 11:45:42 +0200

    Adding the Auth Page

--- basic-project/src/app/app-routing.module.ts
+++ basic-project/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeNoDetailComponent } from './recipes/recipe-no-detail/recipe-no-detail.component';
@@ -28,6 +29,7 @@ const appRouts: Routes = [
     ],
   },
   { path: 'shopping-list', component: ShoppingListComponent },
+  { path: 'auth', component: AuthComponent },
 ];
 
 @NgModule({
