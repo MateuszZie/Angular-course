64d8dd349ba1dfce7ad6a29c584638fb9cd4eb9f
Mateusz Ziebura <mateusz_860@op.pl>
2022-10-01 17:07:00 +0200

    Adding an Auth Guard

--- basic-project/src/app/app-routing.module.ts
+++ basic-project/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeNoDetailComponent } from './recipes/recipe-no-detail/recipe-no-detail.component';
@@ -13,6 +14,7 @@ const appRouts: Routes = [
   {
     path: 'recipies',
     component: RecipesComponent,
+    canActivate: [AuthGuard],
     children: [
       {
         path: 'detail/:id',
