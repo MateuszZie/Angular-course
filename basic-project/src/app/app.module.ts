f0623eeaea303441439a932873cb4076f5333b69
Mateusz Ziebura <mateusz_860@op.pl>
2022-10-02 17:45:44 +0200

    Understanding the Core Module

--- basic-project/src/app/app.module.ts
+++ basic-project/src/app/app.module.ts
@@ -4,19 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { Header } from './header/header.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { DropdownDirective } from './shered/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shoppingList.service';
 import { AppRoutModule } from './app-routing.module';
 import { RecipeService } from './recipes/recipe.service';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthComponent } from './auth/auth.component';
-import { LoadingSppinerComponent } from './shered/loading-spiner/loading-spiner.component';
 import { AuthInterceptorService } from './auth/authInterceptor.service';
-import { AlertComponent } from './shered/alert/alert/alert.component';
-import { PlaceHolderDirective } from './shered/placeholder.directive';
 import { RecipeModule } from './recipes/recipe.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shered/shared.module';
+import { CoreModule } from './core.module';
 
 @NgModule({
   declarations: [AppComponent, Header, AuthComponent],
@@ -29,15 +26,7 @@ import { SharedModule } from './shered/shared.module';
     RecipeModule,
     ShoppingListModule,
     SharedModule,
-  ],
-  providers: [
-    ShoppingListService,
-    RecipeService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptorService,
-      multi: true,
-    },
+    CoreModule,
   ],
   bootstrap: [AppComponent],
 })
