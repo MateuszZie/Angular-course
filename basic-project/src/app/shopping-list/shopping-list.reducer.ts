d9f24c5a254249f2e7f4fe5db6663d997a2f1173
Mateusz Ziebura <mateusz_860@op.pl>
2022-10-06 19:49:04 +0200

    Adding Logic to the Reducer

--- basic-project/src/app/shopping-list/shopping-list.reducer.ts
+++ basic-project/src/app/shopping-list/shopping-list.reducer.ts
@@ -1,7 +1,16 @@
+import { Action } from '@ngrx/store';
 import { Ingridient } from '../shered/ingredient.model';
 
 const initialState = {
   ingredients: [new Ingridient('apples', 5), new Ingridient('tomatoes', 10)],
 };
 
-export function ShoppingListReducer(state = initialState, action) {}
+export function ShoppingListReducer(state = initialState, action: Action) {
+  switch (action.type) {
+    case 'ADD_INGREDIENT':
+      return {
+        ...state,
+        ingredient: [...state.ingredients, action],
+      };
+  }
+}
